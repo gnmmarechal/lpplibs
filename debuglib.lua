d3463ad7e81f4f4e99a8b2bb3a78b156517b3794
gnmmarechal <gnmpolicemata91@gmail.com>
2016-04-26 23:30:30 +0100

    added Debug.takeScreenshot

--- debuglib.lua
+++ debuglib.lua
@@ -1,34 +1,48 @@
 --Part of the LPPLIB Project
 -- https://github.com/gnmmarechal/lpplibs
 -- debuglib
+debuglibversion = 1
+Debug = {}
 
 --Colours
-	Debug.color.white = Color.new(255,255,255)
-	Debug.color.green = Color.new(0,240,32)
-	Debug.color.red = Color.new(255,0,0)
-	Debug.color.black = Color.new(0,0,0)
-	Debug.color.current = Debug.color.white
+	Debug.colorwhite = Color.new(255,255,255)
+	Debug.colorgreen = Color.new(0,240,32)
+	Debug.colorred = Color.new(255,0,0)
+	Debug.colorblack = Color.new(0,0,0)
+	Debug.colorcurrent = Debug.color.white
 	
 function Debug.setDebugColor(colorname) --Sets default debug lib text colour
-	Debug.color.current = colorname
+	Debug.colorcurrent = colorname
+end
+
+function debuglib.require(version)
+	if debuglibversion == version then
+	else
+		System.exit()
+	end
 end
 
 
 --Using code from Eucliod r6 for Debug.getFPS()
-Debug.var.fpstimer = Timer.new()
-Debug.var.fps = 0
-Debug.var.fps_counter = 0
+Debug.varfpstimer = Timer.new()
+Debug.varfps = 0
+Debug.varfps_counter = 0
 function Debug.getFPS() --Gets framerate and returns its value
-	Debug.var.fps_counter = Debug.var.fps_counter + 1
-	if Timer.getTime(Debug.var.fpstimer) > 1000 then
-		Timer.reset(Debug.var.fpstimer)
-		Debug.var.fps = Debug.var.fps_counter
-		Debug.var.fps_counter = 0
+	Debug.varfps_counter = Debug.varfps_counter + 1
+	if Timer.getTime(Debug.varfpstimer) > 1000 then
+		Timer.reset(Debug.varfpstimer)
+		Debug.varfps = Debug.varfps_counter
+		Debug.varfps_counter = 0
 	end
-	return Debug.var.fps
+	return Debug.varfps
 --	gpu_drawtext(160, 220, Debug.var.fps)
 end
 function Debug.drawFPS(DISPLAY) --Draws FPS on either TOP_SCREEN or BOTTOM_SCREEN
-	Debug.var.framerate = Debug.getFPS()
-	Screen.debugPrint(0,0,"FPS:"..Debug.var.framerate, Debug.color.current, DISPLAY)
+	Debug.varframerate = Debug.getFPS()
+	Screen.debugPrint(0,0,"FPS:"..Debug.varframerate, Debug.colorcurrent, DISPLAY)
+end
+function Debug.takeScreenshot()
+	day_value,day,month,year = System.getDate()
+	h,m,s = System.getTime()
+	System.takeScreenshot("/scrshot-"..day_value..day..month..year..h..m..s..".bmp",false)
 end
